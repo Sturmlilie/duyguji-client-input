7b89d21b378978b6fdccde1c7af24fb6746a8ab6
Sturmlilie <jonas.kulla@alumni.fh-aachen.de>
2020-07-12 17:10:51 +0200

    Cleanup imports

--- src/main/java/ancurio/duyguji/client/VanillaShortcodes.java
+++ src/main/java/ancurio/duyguji/client/VanillaShortcodes.java
@@ -1,8 +1,8 @@
 package ancurio.duyguji.client;
 
-import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStream;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
