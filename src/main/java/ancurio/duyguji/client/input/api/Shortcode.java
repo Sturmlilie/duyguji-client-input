149e0c9c6635ec281e6d85f2347aee20c35f3c3f
Sturmlilie <jonas.kulla@alumni.fh-aachen.de>
2020-07-19 20:13:12 +0200

    Start generating javadoc, and document api package

--- src/main/java/ancurio/duyguji/client/input/api/Shortcode.java
+++ src/main/java/ancurio/duyguji/client/input/api/Shortcode.java
@@ -6,12 +6,19 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.function.Consumer;
 
+/**
+ * In essence, this is an extended {@code Pair<String, String>}.
+ */
 public class Shortcode {
-    // The shortcode (without enclosing colons!)
+    /**
+     * The shortcode (without enclosing colons!).
+     */
     public final String code;
 
-    // The represented emoji/symbol; note that this can be more
-    // than one codepoint due to combining characters
+    /**
+     * The represented emoji/symbol; note that this can be more
+     * than one codepoint due to combining characters.
+     */
     public final String symbol;
 
     public Shortcode(final String code, final String symbol) {
@@ -38,10 +45,27 @@ public class Shortcode {
         return new Shortcode(code, symbol);
     }
 
+    /**
+     * @see Shortcode#readPairList(BufferedReader, char, Consumer)
+     *
+     * @param stream the input stream that lines are sourced from.
+     * @param separator the separating character, only the last occurence in a line is considered.
+     * @param consumer the callback receiving parsed Shortcodes.
+     */
     public static void readPairList(final InputStream stream, final char separator, final Consumer<Shortcode> consumer) {
         readPairList(new BufferedReader(new InputStreamReader(stream)), separator, consumer);
     }
 
+    /**
+     * "Pair" referring to Shortcodes.
+     * Reads a plain text stream where each line has the format {@code <symbol><separator><code>},
+     * filtering out invalid lines, and feeds each successfully parsed Shortcode into
+     * {@code consumer}.
+     *
+     * @param reader the file / data stream that lines are sourced from.
+     * @param separator the separating character, only the last occurence in a line is considered.
+     * @param consumer the callback receiving parsed Shortcodes.
+     */
     public static void readPairList(final BufferedReader reader, final char separator, final Consumer<Shortcode> consumer) {
         reader.lines()
             .map(l -> fromPairLine(l, separator))
